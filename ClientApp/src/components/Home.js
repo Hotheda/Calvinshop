fe1b6b3561978072da6dd56b24a15e8be12d0ba9
Heda <hotheda@gmail.com>
2020-02-27 22:12:10 +0100

    Landingpage button

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,14 +1,19 @@
 import React from "react"
 
 function Home(){
-    return(
+    return(    
         <div className="home">
-            <div className="home_header">
-                <h1>Did you know?</h1>
-                <h2>People are suffering</h2>
+            <div className="home_content">                
+                <div className="home_header">
+                    <h1>Did you know?</h1>
+                    <h2>People are suffering</h2>
+                </div>
+                <div className="home_text">
+                    <p>They are 1000 of.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </div>
-            <div className="home_text">
-                <p>They are 1000 of.. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="help_btn">
+                <a href="">How can i help?</a>
             </div>
         </div>
     )
