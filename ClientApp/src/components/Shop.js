a2f9e746af7cad46d0bbd2484041f15fdc3f483c
Heda <hotheda@gmail.com>
2020-03-02 00:10:46 +0100

    added products JSON

--- ClientApp/src/components/Shop.js
+++ ClientApp/src/components/Shop.js
@@ -1,8 +1,24 @@
 import React from "react"
+import products from "./products.json"
 
 function Shop(){
+    const product=products.products.map((item)=>{
+        return (
+            <div key={item.id} className="product_frame">
+                <h3>{item.name}</h3>
+                <img className= "product_img" src={"./img/products/"+item.img}/>
+                <p>{item.description}</p>
+            </div>
+        )
+    })
+
     return(
-        <h1>Here's the shop and all the projects</h1>
+        <div>            
+            <h1 className="shop_header">Here's the shop and all the projects</h1>
+            <div className="products">            
+                {product}
+            </div>
+        </div>
     )
 }
 
