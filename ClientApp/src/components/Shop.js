4b44ffadc43c934c1d5147ccf8920857f200e2c3
Heda <hotheda@gmail.com>
2020-03-06 13:54:13 +0100

    context in cunction

--- ClientApp/src/components/Shop.js
+++ ClientApp/src/components/Shop.js
@@ -1,31 +1,25 @@
-import React from "react"
-import products from "./products.json"
+import React, {useContext} from "react"
 import {ProductsContext} from "./ProductsContext"
 
 
-class Shop extends React.Component{
-    static contextType = ProductsContext
-    
-    render(){
-        console.log(this.context.productsList)
-        const product=this.context.productsList.map((item)=>{
-            return (
-                <div key={item.id} className="product_frame">
-                    <h3>{item.name}</h3>
-                    <img className= "product_img" alt="productimage" src={"./img/products/"+item.img}/>
-                    <p>{item.description}</p>
-                </div>
-            )
-        })
-        return(
-            <div>            
-                <h1 className="shop_header">Products</h1>
-                <div className="products">            
-                    {product}
-                </div>
+function Shop(){
+    const product=useContext(ProductsContext).productsList.map((item)=>{
+        return (
+            <div key={item.id} className="product_frame">
+                <h3>{item.name}</h3>
+                <img className= "product_img" alt="productimage" src={"./img/products/"+item.img}/>
+                <p>{item.description}</p>
             </div>
         )
-    }
+    })
+    return(
+        <div>            
+            <h1 className="shop_header">Products</h1>
+            <div className="products">            
+                {product}
+            </div>
+        </div>
+    )
 }
 
 export default Shop
