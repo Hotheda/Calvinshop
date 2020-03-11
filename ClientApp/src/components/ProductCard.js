d4b011c03a7efd89e4b2f646ee6f8d987208c15d
Heda <hotheda@gmail.com>
2020-03-11 01:21:45 +0100

    Size added to cart

--- ClientApp/src/components/ProductCard.js
+++ ClientApp/src/components/ProductCard.js
@@ -1,37 +1,44 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import {ProductsContext} from "./ProductsContext"
 
 function ProductCard(props){
+    const [seletedSize, setSize]=useState(String(props.item.size[0]) )
     const [/*products*/, /*setProducts*/, cartList, setCartList] = useContext(ProductsContext)
 
     function addToCart(item){
         /***** Check if item is in cart  *****/
         var isInCart=false
+        var newitem = JSON.parse(JSON.stringify(item))
+        newitem.size = seletedSize
+        newitem.id = item.id+"-"+seletedSize
+
         if(cartList.length!==0){
             cartList.forEach(product => {
-                if(item.id===product.id){
+                if(newitem.id===product.id){
                     product.inCart++;
                     isInCart=true
+                    alert(item.name+" added to cart")
                 }
             });
         }
         if(!isInCart){
-            setCartList([...cartList, item])
+            setCartList([...cartList, newitem])
             alert(item.name+" added to cart")
         }
+        console.log(newitem)
     }
 
     function onSizeChange(e){
-        console.log(e)
+        setSize(e.target.value)
     }
 
-    return (
+    return (        
         <div className="product_frame">
             <h3>{props.item.name}</h3>
             <img className= "product_img" alt="productimage" src={"./img/products/"+props.item.img}/>
             <p>{props.item.description}</p>
             <p><strong>{props.item.price}$</strong></p>
-            <select onChange={()=>onSizeChange}>
+            <select value={seletedSize} onChange={(e)=>onSizeChange(e)}>
                 {props.item.size.map((myItem=>{
                     return( <option key={myItem} value={myItem}>{myItem}</option> )
                 }))}
