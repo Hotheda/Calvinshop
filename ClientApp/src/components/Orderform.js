0ead048a4c33b19243cf83d2a1aaeb402b29c3ca
Heda <hotheda@gmail.com>
2020-04-26 23:11:20 +0200

    Chackout form for name and adress added

--- ClientApp/src/components/Orderform.js
+++ ClientApp/src/components/Orderform.js
@@ -1,7 +1,33 @@
 import React from "react"
 
 export default function Orderform(){
+
+    const handleclick = (e)=>{
+        e.preventDefault()
+        console.log("Click event")
+    }
+
     return(
-        <p>Orderform</p>
+        <div>
+            <p>Orderform</p>
+            <form>
+                <div>
+                    <p>Name</p>
+                    <input id="firstName" placeholder="First name"/>
+                    <input id="lastName" placeholder="Last name"/>
+                    <br/>
+                </div>
+                <p>Adress</p>
+                <input id="customerAdress" placeholder="Street adress"/>
+                <p>Zip code</p>
+                <input id="zipCode" placeholder="Zip code"/>
+                <p>City</p>
+                <input id="city" placeholder="City"/>
+                <p>E-mail</p>
+                <input id="eMail" placeholder="E-mail"/>
+                <br/>
+                <button onClick={handleclick}>Comfirm order</button>
+            </form>
+        </div>
     )
 }
