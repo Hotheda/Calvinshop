c23f1805c787e1f63f9137e17043ac43cac2523b
Heda <hotheda@gmail.com>
2020-04-29 13:21:12 +0200

    Added details to checkout page

--- ClientApp/src/components/Confirm.js
+++ ClientApp/src/components/Confirm.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React, { useContext } from "react"
+import { ProductsContext } from "./ProductsContext"
 
 export default function Confirm(props){
+const [ /* */ , /* */, cart, /**/] = useContext(ProductsContext)
+
     console.log(props)
+    const userData = props.location.state
+    const userCart = cart.map((item)=>{
+        return(
+            <div key={item.id}>
+                <p>product id: {item.id}</p>
+                <p>{item.inCart} item of {item.name} size: {item.size}</p>
+                <br/>
+            </div>
+        )        
+    })
     return(
         <div>
             <p>Confirmed</p>
-            {props.location.state.firstname}
+            <p>{userData.firstname} {userData.lastname}</p>
+            <p>{userData.adress}</p>
+            <p>{userData.email}</p>
+            <br/>
+            {userCart}
         </div>
     )
 }
