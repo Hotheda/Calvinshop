027ef0f3b9959eb4a6fe92b9324d1e773726ad21
Heda <hotheda@gmail.com>
2020-04-30 15:25:54 +0200

    Order complete page added

--- ClientApp/src/components/Confirm.js
+++ ClientApp/src/components/Confirm.js
@@ -1,28 +1,68 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { ProductsContext } from "./ProductsContext"
 
-export default function Confirm(props){
-const [ /* */ , /* */, cart, /**/] = useContext(ProductsContext)
 
-    console.log(props)
+/*
+    TODO:
+    Add confirm look to page and clear shopping cart (or not succeded and dont clear cart)
+    Send mail to user and site owner with usr details and order
+    Paypal integration
+*/
+
+export default function Confirm(props){
+const [ /* */ , /* */, cart, setCart] = useContext(ProductsContext)
+
+    const [orderConfirmed,setOrderConfirmed] = useState(false);
+    const [orderItems, /*setOrderItems*/] = useState(cart)
+    let totalPrice = 0;
+    let shippingPrice = 15;
+
     const userData = props.location.state
-    const userCart = cart.map((item)=>{
+
+    let userCart = orderItems.map((item)=>{
         return(
-            <div key={item.id}>
-                <p>product id: {item.id}</p>
+            <li key={item.id}>
+                <p>product id: {item.id} - {item.price}$</p>
                 <p>{item.inCart} item of {item.name} size: {item.size}</p>
-                <br/>
-            </div>
-        )        
+            </li>
+        )
     })
+
+
+    const handleClick = (e)=>{
+        setOrderConfirmed(!orderConfirmed)
+
+        // If payment success then delete shoppingcart items
+        //render products before deleting
+        setCart([]);
+    }
+
+    const calculateTotal = () =>{
+        totalPrice = cart.reduce((total, item)=>{
+            return total + item.price;
+        },0)
+        totalPrice=totalPrice+shippingPrice;
+        return totalPrice;
+    }
+
+    
+    calculateTotal()
     return(
         <div>
-            <p>Confirmed</p>
+            {orderConfirmed ? <h1>Confirmed</h1> : <p>Check your credentials and confirm order</p>}
             <p>{userData.firstname} {userData.lastname}</p>
             <p>{userData.adress}</p>
+            <p>{userData.zipcode}</p>
+            <p>{userData.city}</p>
             <p>{userData.email}</p>
-            <br/>
-            {userCart}
+            <hr/>
+            <ol>
+                {userCart}
+            </ol>
+            <p>Shipping: {shippingPrice}$</p>
+            <hr/>
+            <p>Total: {totalPrice}$</p>
+            {!orderConfirmed && <button onClick={handleClick}>Confirm order</button>}
         </div>
     )
 }
