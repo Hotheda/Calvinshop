e3dac95bfa122f2732ebc40e35ca1eb9dbcd84b7
Heda <hotheda@gmail.com>
2020-05-14 14:16:31 +0200

    Mobile menu done

--- ClientApp/src/components/Navbar.js
+++ ClientApp/src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import {Link} from "react-router-dom"
 import { ProductsContext } from "./ProductsContext"
 
 function Navbar(){
     const [/* */,/* */,cartList, /* */] = useContext(ProductsContext)
+    const [navbarOpen, setNavbarOpen] = useState(false)
+    const [navbarClass, setNavbarClass] = useState("navlinks_close")
+
+    const toggleMenu=()=>{
+        if(navbarOpen)
+            setNavbarClass("navlinks_close")
+        else
+            setNavbarClass("navlinks_open")
+        setNavbarOpen(!navbarOpen)
+    }
     
     // Count number of items in cart
     const itemsInCart = cartList.reduce((number, item)=>{
@@ -15,7 +25,12 @@ function Navbar(){
             <div className="navLogo">
                 <Link className="logoLink" to="./">LoGO</Link>
             </div>
-            <div className="navlinks">
+            <div className="nav_button" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div onClick={toggleMenu} className={navbarClass}>
                 <Link className="link" to="./about">About</Link>
                 <Link className="link" to="./shop">Shop</Link>
                 <Link className="cart_link" to="./cart">Cart: <span>{itemsInCart}</span></Link>
